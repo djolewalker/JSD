195d691f0409803a66144a4324a0cccb54cd421d
Dimitrije <dimitrije24000@gmail.com>
2024-01-29 17:23:35 +0100

    Fix file paths in setup tools

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,13 +1,14 @@
 from textx import metamodel_from_file, language
 import os
 
-resume_metamodel_path = 'src/meta_model/resume.tx'
-metamodel = metamodel_from_file(resume_metamodel_path)
+resume_metamodel_path = 'meta_model/resume.tx'
 
 
 def parse_resume(file_path):
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"The resume file was not found at location {file_path}.")
+
+    metamodel = metamodel_from_file(resume_metamodel_path)
 
     return metamodel.model_from_file(file_path)
 
